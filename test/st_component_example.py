57110966d17fd4946fd55438cbd02c926cf21b2c
Ray <lanskyassassination@gmail.com>
2025-02-02 11:09:29 +0800

    feat(editor): enhance readonly mode handling and improve data return logic

--- test/st_component_example.py
+++ test/st_component_example.py
@@ -1,8 +1,20 @@
 from streamlit_editorjs_component import streamlit_editorjs
 import streamlit as st
 
+st.set_page_config(layout="wide")
 st.title("EditorJS")
-content = streamlit_editorjs(height=500, readonly=False)
 
-if st.button("Get content"):
+st.write("### Editable mode")
+col1, col2 = st.columns(2)
+with col1:
+    content = streamlit_editorjs(height=300)
+with col2:
+    st.write(content)
+
+st.write("---")
+st.write("### Readonly mode")
+col1, col2 = st.columns(2)
+with col1:
+    content = streamlit_editorjs(data=content, height=300, readonly=True)
+with col2:
     st.write(content)
